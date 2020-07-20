3e31da14e67eff7e48ed2be0fb27bd0f6ff50ab1
akash <akashincrp@gmail.com>
2020-07-20 19:34:59 +0530

    protected

--- index.js
+++ index.js
@@ -2,12 +2,30 @@ const express = require('express')
 const app = express();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 
-passport.use(new GoogleStrategy())
+passport.use(new GoogleStrategy({
+    clientID : keys.googleClientID,
+    clientSecret : keys.googleClientSecret,
+    callbackURL : '/auth/google/callback'
+}, () => {
+    console.log(accessToken);
+})
+);
 
-// 659681052108-n9ae1evna2fpbk3sb7uf4nb8kdocimtd.apps.googleusercontent.com
-// e7q93fvn2989rlzSs0HQyrUT
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+})
+);
+
+app.get('/auth/google/callback', 
+    passport.authenticate( 'google', { 
+        successRedirect: '/auth/google/success',
+        failureRedirect: '/auth/google/failure'
+})
+);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(5000);
